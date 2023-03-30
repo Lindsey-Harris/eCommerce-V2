77b9c1cc67e4d812b4e4041d0ab7b62fa8e80cde
Lindsey-Harris <h.lindsey16@yahoo.com>
2023-03-30 12:37:35 -0400

    committing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import '../components/navbar.css';
+import '../pages/cart/cart-icon.css';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import { ShoppingCart } from 'phosphor-react';
+import { ShopContext } from "../context/shop-context";
 
 
-  function Navbar() {
+function Navbar() {
+  const { cartItems } = useContext(ShopContext);
+
+  //calculate total number of items in cart
+  const totalItems = Object.values(cartItems).reduce((acc, curr) => acc + curr, 0);
     return (
       <div className="navbar">
         <ul>
@@ -12,7 +18,8 @@ import { ShoppingCart } from 'phosphor-react';
           <li><i className="fas fa-shopping-cart"></i><Link to="/products">Products</Link></li>
           <li><i className="fas fa-pen"></i><Link to="/contact">Contact</Link></li>
           <Link to="/cart">
-            <ShoppingCart size={25} />
+            <ShoppingCart size={25} className="cart-icon" />
+            {totalItems > 0 && <span className="cart-indicator">{totalItems}</span>}
           </Link>
         </ul>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React from "react";
+import '../components/home.css';
+import slide1 from "../images/slide1.webp";
+import slide2 from "../images/slide2.webp";
+import slide3 from "../images/slide3.webp";
+import slide4 from "../images/slide4.webp";
+import slide5 from "../images/slide5.webp";
+import slide6 from "../images/slide6.webp";
+import masterCard from "../images/mastercard-visa.jpeg";
+import watch from "../images/watchHome.jpeg";
+
 
 function Home() {
-    return (
-        <h1>this is the homepage</h1>
-    );
+  return (
+    <>
+      <section>
+        <div className="slide-container">
+          <div className="image-container">
+            <img src={slide1} alt="apple watches" />
+            <img src={slide2} alt="apple watches" />
+            <img src={slide3} alt="phone holder" />
+            <img src={slide4} alt="phone holder" />
+            <img src={slide5} alt="apple bands" />
+            <img src={slide6} alt="phone holder" />
+          </div>
+        </div>
+      </section>
+
+      <section id="flex-container">
+        <div className="exploreMore">
+          <img src={masterCard}alt="credit/debit cards" />
+          <a href="/products">
+            <button className="btn">Explore More</button>
+          </a>
+        </div>
+        <div className="exploreMoree">
+          <img src={watch} alt="apple watches" />
+        </div>
+      </section>
+
+      <div className="contained">
+        <p>
+          Electronically Yours was created for individuals who love electronics.
+          I hope you enjoy!!
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default Home;
